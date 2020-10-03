1d6f550214fc12838521b6234deb93b50a3f4c5f
Karl Mueller <muellerkarlw@gmail.com>
2020-10-03 18:26:15 -0400

    Added ambulance model [basic]

---
+++ ambulance_model.py
@@ -0,0 +1,9 @@
+class Ambulance:
+    def __init__(self, x = 0, y = 0, vel = 0, gas=[])
+        self.x = x
+        self.y = y
+        self.vel = vel
+        self.gas = gas
+
+    def getdata(self)
+        print(f'({self.x},{self.y}) with {self.vel} m/s and {self.gas} L fuel')
