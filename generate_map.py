259d9e97208acc2b001649af01c00994368693de
karl_mueller_w <muellerkarlw@gmail.com>
2020-10-03 15:03:15 -0400

    Added initial class for road matrix and road nodes

---
+++ generate_map.py
@@ -0,0 +1,13 @@
+'''
+This function intended to generate an mxn matrix of nodes to represent possible positions of the ambulance.
+The ambulance may move to an adjacent node (diagonals okay, so 8 movement options)
+Each node has a predefined position 
+Travel time and distance are calculated based on the speed of the 
+
+'''
+
+import numpy as np
+
+def generate_map(input_m, input_n):
+    road_mat = numpy.full((input_m,input_n_, Road_Node()))
+    
---
+++ road_node.py
@@ -0,0 +1,11 @@
+class Road_Node:
+    def __init__(self, x_in=0, y_in=0, traf_in=0)
+        self.x = x_in
+        self.y = y_in
+        self.traffic = traf_in
+
+    
+
+
+    def get_data(self):
+        print(f'debug/({self.x},{self.y}) with {self.traffic} traffic')
