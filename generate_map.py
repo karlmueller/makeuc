18b8483a8652e60415a2508f38ca6017acfffb06
Karl Mueller <muellerkarlw@gmail.com>
2020-10-03 18:16:00 -0400

    Completed node map, begin localization

--- generate_map.py
+++ generate_map.py
@@ -5,9 +5,9 @@ Each node has a predefined position
 Travel time and distance are calculated based on the speed of the 
 
 '''
-
 import numpy as np
-from np import random as rd
+from numpy import random
+import matplotlib.pyplot as plt
 
 
 class Road_Node:
@@ -16,17 +16,31 @@ class Road_Node:
         self.y = y_in
         self.traffic = traf_in
 
-    def get_data(self):
-        print(f'debug/({self.x},{self.y}) with {self.traffic} traffic')
+    #def get_data(self):
+    #    print(f'debug/({self.x},{self.y}) with {self.traffic} traffic')
 
-
+'''
 def generate_map(input_m, input_n):
     road_mat = np.zeros((input_m, input_n))
     for x in range(0, input_m):
         for y in range(0, input_n):
-            road_mat[x][y] = Road_Node(100*x+40*random.random(),100*y+40*random.random(),random.uniform(0,20))
-
-
-
+            road_mat[x][y] = Road_Node()
+# 100*x+40*random.rand(),100*y+40*random.rand(),random.uniform(0,20)
 qq = generate_map(10,10)
 print(qq)
+'''
+
+def generate_map(input_m, input_n):
+    rmp = []
+    for x in range(0, input_m):
+        for y in range(0, input_n):
+            rmp.append(Road_Node(100*x+40*random.rand(), 100*y+40*random.rand(), random.uniform(0, 20)))
+    return rmp
+
+roadmap_list = generate_map(10, 10)
+
+for obj in roadmap_list:
+    #print(f'{obj.x},{obj.y}')
+    plt.scatter(obj.x,obj.y)
+
+plt.show() #shows the locations of the intersections of the road
---
+++ location.py
@@ -0,0 +1,10 @@
+''' purpose of this script is to check the position of the ambulance
+relative to the nodes in the system.  This shall write the current node position of the ambulance 
+to the beginning node and will write the 8 surrounding nearest neighbor nodes to a list
+'''
+
+#current position of ambulance
+xpos = amb.x #represented not as the position in m shown in the nodes, but as the 1:length index value
+ypos = amb.y
+
+
